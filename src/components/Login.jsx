dea170a6818e8fdc597a7e885f16248a4f27ccac
Dominika <werkamdor@gmail.com>
2022-12-23 21:41:41 +0100

    Changes in POST method

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,25 @@
 import './login.css'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { login } from '../crud/login'
 
 const Login = ({ open, onClose }) => {
 
 
-    const [logData, setLogData] = useState({
-        login: "",
-        password: ""
-    })
+  const [logData, setLogData] = useState({
+    nick: "",
+    password: "",
+  })
 
 
-const handleLogin = () => {
-        login(logData)
-        .then(()=>{
-            console.log("jej")
-        })
-        .catch((error)=>{
-            console.log("dupa",error.response.data.title)
-        })
-      }
+  const handleLogin = () => {
+    login(logData)
+      .then(() => {
+        console.log("jej")
+      })
+      .catch((error) => {
+        console.log("dupa", error.response.data.title)
+      })
+  }
   if (!open) return null;
   return (
     <div onClick={onClose} className='overlay'>
@@ -27,37 +27,40 @@ const handleLogin = () => {
         onClick={(e) => {
           e.stopPropagation();
         }}
-        >
+      >
         <div>
           <div className='logContainer'>
-          <div className='closeBtn' onClick={onClose}>
-            X
-          </div>
-          <div className="formContainer">
-            <h1>Logowanie</h1>
-            <div className='logForm'>
-            <form className='login'>
-                <input className='loginInput'
+            <div className='closeBtn' onClick={onClose}>
+              X
+            </div>
+            <div className="formContainer">
+              <h1>Logowanie</h1>
+              <div className='logForm'>
+                <form className='login' onSubmit={(event) => event.preventDefault()}>
+                  <input className='loginInput'
                     placeholder="Login"
-                    value={logData.login}
-                    onChange={(e) => setLogData({ ...logData, login: e.target.value })}
-                />
-                <input className='loginInput'
+                    value={logData.nick}
+                    onChange={(e) => setLogData({ ...logData, nick: e.target.value })}
+                  />
+                  <input className='loginInput'
                     placeholder="Hasło"
                     type="password"
                     value={logData.password}
                     onChange={(e) => setLogData({ ...logData, password: e.target.value })}
-                />
-                <input className="loginButton" type="button" value="Zaloguj" onClick={handleLogin}/>
-            </form>
+                  />
+                  <button className="loginButton" 
+                    onClick={handleLogin}>
+                      Zaloguj
+                  </button>
+                </form>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div> 
       </div>
     </div>
-   
-      
+
+
   );
 };
 
