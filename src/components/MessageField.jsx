51bfe18fea2c915fe53b2477f74b02a588ea206a
Dominika <werkamdor@gmail.com>
2023-01-22 13:02:52 +0100

    Messages recieving fixed

--- src/components/MessageField.jsx
+++ src/components/MessageField.jsx
@@ -4,7 +4,7 @@ import "./MessageField.css";
 import { RiSendPlaneLine } from "react-icons/ri";
 import { CgClose } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { sendMessage } from "../store/actions";
 import UserInfo from "./UserInfo";
 import { getUserDataFromNick } from "../crud/getUserDataFromNick";
@@ -16,6 +16,12 @@ function MessageField() {
   const dispatch = useDispatch();
   const messages = useSelector((state) => state?.messages);
   const [openModal,setOpenModal]=useState(false)
+  const messagesEndRef = useRef(null)
+ 
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+  
   useEffect(()=>{
     getUserDataFromNick(user)
     .then((resp)=>{
@@ -27,6 +33,19 @@ function MessageField() {
       alert(err.response.data.title?err.response.data.title:"Wystąpił nieznany błąd")
     })
   },[user])
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages]);
+
+  const onEnterPress = (e) => {
+    if(e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      if (message) {
+        dispatch(sendMessage(message)).then(() => setMessage(""));
+      }
+    }
+  }
 
   //console.log("wiadomości", messages)
   return (
@@ -42,14 +61,17 @@ function MessageField() {
           <CgClose style={{ fontSize: "25", color: "#535353" }} />
         </button>
       </div>
-      <div className="conversation">
+      <div className="conversation" >
         { messages.length==0?<p style={{fontStyle:"italic"}}>Rozpocznij konwersację</p>:messages.map((message) => (<Message message={message} />))}
+        <div ref={messagesEndRef} />
       </div>
       <div className="newMessageField">
         <textarea
           className="newMessageInput"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={onEnterPress}
+
         />
         <div
           className="send"
